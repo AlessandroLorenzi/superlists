72a5ec8c9d40d990e7b9fe158ad3d7ee5e4f498b
Alessandro Lorenzi <me@alorenzi.eu>
2022-06-26 22:35:43 +0200

    test: add full jurney functional test

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,6 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,9 +14,48 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_home_page_title(self):
+        # Checkout the homepage
         self.browser.get("http://localhost:8000")
 
+        # check the title and header is t-do lists
         self.assertIn("To-Do lists", self.browser.title)
+        header_test = self.browser.find_element(By.TAG_NAME, "h1")
+
+        self.assertIn("To-Do lists", header_test.text)
+
+        # she is invited to enter a to-do item
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        # she types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys("Buy peacocks feathers")
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_element(By.TAG_NAME, "tr")
+
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+
+        # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn(
+            "2: Use peacock feathers to make a fly", [row.text for row in rows]
+        )
+
+        # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for her -- there is some explanatory text to that effect.
+        self.fail("Finish the test!")
 
 
 if __name__ == "__main__":
