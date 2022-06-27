081150d2edf0b33f70f18652d2e8e4a3cc029ce7
Alessandro Lorenzi <me@alorenzi.eu>
2022-06-27 18:25:03 +0200

    test: new function helpers

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -28,34 +28,27 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # she types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
-        inputbox.send_keys("Buy peacocks feathers")
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-
-        time.sleep(1)
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_element(By.TAG_NAME, "tr")
-
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+        self.send_new_item("Buy peacock feathers")
+        self.check_for_row_in_list("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
-
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-            "2: Use peacock feathers to make a fly", [row.text for row in rows]
-        )
+        self.send_new_item("Use peacock feathers to make a fly")
+        self.check_for_row_in_list("1: Buy peacock feathers")
+        self.check_for_row_in_list("2: Use peacock feathers to make a fly")
 
         # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for her -- there is some explanatory text to that effect.
         self.fail("Finish the test!")
+
+    def check_for_row_in_list(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def send_new_item(self, new_item):
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys(new_item)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
