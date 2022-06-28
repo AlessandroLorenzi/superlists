9d3b89057c715542ee39fa039c04fa2b4a819970
Alessandro Lorenzi <me@alorenzi.eu>
2022-06-28 16:15:23 +0200

    feat: show all items and redirect

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -27,6 +28,43 @@ class SmokeTest(TestCase):
         self.assertTemplateUsed("lists/home.html")
 
     def test_can_save_a_post_request(self):
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_dont_save_empty_items(self):
+        self.client.post("/", data={"item_text": ""})
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_redirect_after_save_item(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode("utf8"))
-        self.assertTemplateUsed("lists/home.html")
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    def test_save_and_retrieve_item(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "The second list item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, "The first (ever) list item")
+        self.assertEqual(saved_items[1].text, "The second list item")
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from lists.models import Item
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return render(
-        request, "lists/home.html", {"new_item_text": request.POST.get("item_text", "")}
-    )
+    if request.method == "POST":
+        save_item(request)
+        return redirect("/")
+
+    items = Item.objects.all()
+    return render(request, "lists/home.html", {"items": items})
+
+
+def save_item(request):
+    item = Item()
+    new_item_text = request.POST.get("item_text", "")
+    if new_item_text != "":
+        item.text = new_item_text
+        item.save()
